0c44b7baa772254340c3844c497e61f52fa22160
Ruan Alexandre dos Santos Campos <ruamcampos366@gmail.com>
2026-04-08 22:45:53 -0300

    feat: setup Angular com estrutura de features e rotas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'products', pathMatch: 'full' },
-  { path: 'products', loadComponent: () => import('./products/products-list/products-list.component').then(m => m.ProductsListComponent) },
-  { path: 'invoices', loadComponent: () => import('./invoices/invoice-list/invoice-list.component').then(m => m.InvoiceListComponent) },
+  { path: 'products', loadComponent: () => import('./products/products-list/products-list').then(m => m.ProductsListComponent) },
+  { path: 'invoices', loadComponent: () => import('./invoices/invoice-list/invoice-list').then(m => m.InvoiceListComponent) },
 ];
